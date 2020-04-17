a9be833f67c3572aefa49f28fdc4c61820a78524
Luke <olooook@outlook.com>
2020-04-17 10:53:39 +0800

    .

--- train.py
+++ train.py
@@ -5,4 +5,4 @@ from tools.utils import load_settings
 
 if __name__ == '__main__':
 	item = sys.argv[1].lower() if len(sys.argv) >= 2 else 'debug'
-	trainer = ASTGCNTrainer(load_settings(item)).run()
+	ASTGCNTrainer(load_settings(item)).run()
